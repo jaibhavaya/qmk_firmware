0b59dbb627eeb1b32ebc1f1eaa3977fb41517672
Jacob Lawson <jaibhavaya@gmail.com>
2020-05-04 22:59:05 -0400

    adding amj40 keymap

---
+++ keyboards/amj40/keymaps/jaibhavaya/keymap.c
@@ -0,0 +1,135 @@
+
+
+
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
+
+
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+        /* Default Layer
+     * ,-----------------------------------------------------------.
+     * | Esc|  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P | BS |
+     * |-----------------------------------------------------------|
+     * |LCtl/Tab| A | S | D |  F |  G |  H |  J |  K |  L | Ent    |
+     * |-----------------------------------------------------------|
+     * | LSft   |  Z |  X |  C |  V |  B |  N |  M | ./> | /? |RSft|
+     * |-----------------------------------------------------------|
+     * | LAlt | LCtl| LGui|  spc fn0  |  spc fn1    | [  |  ] | \  |
+     * `-----------------------------------------------------------'
+     */
+    [_QWERTY] = LAYOUT( \
+        KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
+        MT(MOD_LCTL, KC_TAB),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,\
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_DOT, KC_SLSH,  KC_RSFT,\
+        KC_LALT, KC_LCTL, KC_LGUI, LT(_LOWER, KC_SPC),LT(_RAISE, KC_SPC),KC_LBRC, KC_RBRC, KC_BSLS \
+		),
+
+    /* Function Layer 1 HHKB style
+     * ,-----------------------------------------------------------.
+     * |   `  |   !   |  @  | # | $ | % | ^ | & | * | ( | ) | Bkspc|
+     * |-----------------------------------------------------------|
+     * |     |  |  |  |  |   | Left  | Down  | Up | Right |        |
+     * |-----------------------------------------------------------|
+     * |        |    |    |    |    |    |    |    |    |     |    |
+     * |-----------------------------------------------------------|
+     * |     |    |     |           |             |      |   |     |
+     * `-----------------------------------------------------------'
+     */
+    [_LOWER] = LAYOUT( \
+        KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+		_______,   _______,   _______,   _______,   _______,   _______,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, \
+		_______,   _______,   _______,   _______,  _______,  _______,_______, _______, _______,_______, _______, \
+		_______, _______, _______, _______, _______, _______, _______, _______ \
+		),
+	
+	/* Function Layer 1 HHKB style
+     * ,-----------------------------------------------------------.
+     * | `/~ | 1  | 2  | 3  |  4 |  5 |  6 |  7 |  8 | 9 | 0 | Del |
+     * |-----------------------------------------------------------|
+     * |      |    |    |    |    |   |   |   | ;  | '   |         |
+     * |-----------------------------------------------------------|
+     * |        | |  |    |  + |  - | | - | =  |   ,    |     |
+     * |-----------------------------------------------------------|
+     * |     |    |     |           |             | Stop| App|     |
+     * `-----------------------------------------------------------'
+     */
+    [_RAISE] = LAYOUT( \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+		_______,   _______,   _______,   _______,   _______,   _______,   _______, _______,  KC_SCLN, KC_QUOT, _______, \
+		_______,   _______,   _______,   _______,  _______,  _______,  KC_MINS, KC_EQL, KC_COMM, _______, _______, \
+		_______, _______, _______, _______, _______, _______, _______, _______ \
+		),
+	
+	/* Function Layer 1 HHKB style
+     * ,-----------------------------------------------------------.
+     * |Caps|    |MSel| ⏮ | ⏯ | ⏭ |PSCR|SkLk|Paus|  ↑ | Ins| Del|
+     * |-----------------------------------------------------------|
+     * |      | 🔇 | ⏏ |    |  * |  / |Hone|PgUp|  ← |  → |       |
+     * |-----------------------------------------------------------|
+     * |        | 🔉 | 🔊 |    |  + |  - | End|PgDn| ↓ |     |    |
+     * |-----------------------------------------------------------|
+     * |     |    |     |           |             | Stop| App|RESET|
+     * `-----------------------------------------------------------'
+     */
+    [_ADJUST] = LAYOUT( \
+        _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+		_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______,  _______,  \
+		_______, _______, _______, RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD, \
+		KC_SYSTEM_SLEEP, _______, _______, _______, _______, _______, _______, RESET \
+		),
+
+    
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+ 
+  switch (keycode) {
+    
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
