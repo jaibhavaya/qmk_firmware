73aefcd45f6c462a52d24f2ab24bf516a1128887
Jacob Lawson <jaibhavaya@gmail.com>
2020-05-08 20:09:20 -0400

    updating amj40

--- keyboards/amj40/keymaps/jaibhavaya/keymap.c
+++ keyboards/amj40/keymaps/jaibhavaya/keymap.c
@@ -28,34 +28,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |LCtl/Tab| A | S | D |  F |  G |  H |  J |  K |  L | Ent    |
      * |-----------------------------------------------------------|
-     * | LSft   |  Z |  X |  C |  V |  B |  N |  M | ./> | /? |RSft|
+     * | LSft   |  Z |  X |  C |  V |  B |  N |  M |  ;  | /? |RSft|
      * |-----------------------------------------------------------|
-     * | LAlt | LCtl| LGui|  spc fn0  |  spc fn1    | [  |  ] | \  |
+     * | LAlt | LCtl| LGui|    fn0  |    spc        | [  |  ] | \  |
      * `-----------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT( \
         KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
         MT(MOD_LCTL, KC_TAB),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,\
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_DOT, KC_SLSH,  KC_RSFT,\
-        KC_LALT, KC_LCTL, KC_LGUI, LT(_LOWER, KC_SPC),LT(_RAISE, KC_SPC),KC_LBRC, KC_RBRC, KC_BSLS \
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_SCLN, KC_SLSH,  KC_RSFT,\
+        KC_LALT, KC_LCTL, KC_LGUI, _LOWER,KC_SPC,KC_LBRC, KC_RBRC, KC_BSLS \
 		),
 
     /* Function Layer 1 HHKB style
      * ,-----------------------------------------------------------.
-     * |   `  |   !   |  @  | # | $ | % | ^ | & | * | ( | ) | Bkspc|
+     * |   `  |   1   |  2  | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | Bkspc|
      * |-----------------------------------------------------------|
      * |     |  |  |  |  |   | Left  | Down  | Up | Right |        |
      * |-----------------------------------------------------------|
-     * |        |    |    |    |    |    |    |    |    |     |    |
+     * |        |    |    |    |    | -  |=   | ,   |.  | '   |    |
      * |-----------------------------------------------------------|
-     * |     |    |     |           |             |      |   |     |
+     * |RESET |    |    |           |             |      |   |     |
      * `-----------------------------------------------------------'
      */
     [_LOWER] = LAYOUT( \
-        KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
+        KC_GRV, KC_1, KC_2,   KC_3, KC_4,  KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, \
 		_______,   _______,   _______,   _______,   _______,   _______,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, \
-		_______,   _______,   _______,   _______,  _______,  _______,_______, _______, _______,_______, _______, \
-		_______, _______, _______, _______, _______, _______, _______, _______ \
+		_______,   _______,   _______,   _______,  _______,  KC_MINUS,KC_EQL, KC_COMM, KC_DOT,KC_QUOT, _______, \
+		RESET, _______, _______, _______, _______, _______, _______, _______ \
 		),
 	
 	/* Function Layer 1 HHKB style
